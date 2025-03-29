b064204f554af8cb8880ad55d7286ded8a8330c0
ShibCode <100206484+ShibCode@users.noreply.github.com>
2025-03-29 09:31:13 +0500

    fix one small thing

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto flex max-w-[80vw] justify-center pb-[14vw] md:max-w-[64vw] md:pb-[12vw]">
+    <footer className="mx-auto flex max-w-[80vw] justify-center pb-[14vw] md:max-w-[64vw] md:pb-[12vw] lg:pb-0">
       <div className="flex w-full flex-col items-center justify-between gap-[1.5vw] py-[1.625vw] font-eczar text-[2.5vw] sm:text-[1.75vw] md:text-[1.25vw] lg:flex-row lg:text-[0.75vw]">
         <div className="flex flex-wrap justify-center gap-[0.5em]">
           <p className="opacity-40">
